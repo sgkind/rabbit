5acc0ad012efbb03c9ebf8a51e824d5a9fad04ae
sgkind <sunguanke11@mails.ucas.edu.cn>
2022-12-17 22:41:44 +0800

    add more shortcut

--- lua/config/keybindings.lua
+++ lua/config/keybindings.lua
@@ -1,8 +1,34 @@
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
+-- normal模式下上下“翻页”
+map("n", "<C-u>", "9k", opts)
+map("n", "<C-d>", "9j", opts)
+
+-- visual模式下连续缩进代码
+map("v", "<", "<gv", opts)
+map("v", ">", ">gv", opts)
+
+-- 分屏相关
+map("n", "sv", ":vsp<CR>", opts)  -- 水平分屏
+map("n", "sh", ":sp<CR>", opts)   -- 垂直分屏
+map("n", "sc", "<C-w>c", opts)    -- 关闭当前分屏
+map("n", "so", "<C-w>o", opts)    -- 关闭其他分屏
+-- 分屏比例控制
+map("n", "s>", ":vertical resize +20<CR>", opts)
+map("n", "s<", ":vertical resize -20<CR>", opts)
+map("n", "s=", "<C-w>=", opts)
+map("n", "sj", ":resize +10<CR>", opts)
+map("n", "sk", ":resize -10<CR>", opts)
+-- 分屏间跳转
+map("n", "<leader>h", "<C-w>h", opts)
+map("n", "<leader>j", "<C-w>j", opts)
+map("n", "<leader>k", "<C-w>k", opts)
+map("n", "<leader>l", "<C-w>l", opts)
 
 map("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
 map("n", "<leader>o", ":NvimTreeFocus<CR>", opts)
