f79a4055a05754c5e31cc3b4d7581562cdb39021
sgkind <sunguanke11@mails.ucas.edu.cn>
2022-12-18 10:36:39 +0800

    update nvim-tree configuration

--- lua/config/others.lua
+++ lua/config/others.lua
@@ -3,7 +3,22 @@ vim.opt.termguicolors = true
 -- for nvim-tree
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-require("nvim-tree").setup({})
+require("nvim-tree").setup({
+	-- 关闭文件时，自动关闭
+	auto_close = true,
+  filters = {
+		custom = {
+		  ".git/"
+		},
+		exclude = {
+	    ".gitignore"
+		},
+		dotfiles = true
+	},
+	git = {
+		enable = true
+	}
+})
 
 -- for indent-blankline
 vim.opt.list = true
