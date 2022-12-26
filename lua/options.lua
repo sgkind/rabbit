5bad1f5c6a37dcc90245a422ea0d39eb920de7ce
guanke.sun <guanke.sun@horizon.ai>
2022-12-26 20:42:25 +0800

    use spaces instend of tabs

--- lua/options.lua
+++ lua/options.lua
@@ -1,7 +1,9 @@
 vim.opt.number = true
 vim.opt.showmatch = true
-vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
 vim.opt.encoding = 'utf-8'
 vim.opt.hlsearch = true
 vim.opt.autoindent = true
